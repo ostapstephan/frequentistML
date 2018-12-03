7046a4d07c531178a1a9e6391e530fffa6d944a9
Ostap <ostapstephan@gmail.com>
2018-12-02 21:26:54 -0500

    starting Final

---
+++ keeneKaggleFinal/final.py
@@ -0,0 +1,20 @@
+
+'''
+To get an A on this project, I'm going to request you do the following on this dataset.
+
+Linear regression with L1 penalties
+Linear regression with L2 penalties
+Random Forest
+XGBoost tree or something similar 
+Try a dimensionality reduction method on all, or a subset of your data 	
+Try a feature selection method.
+Try one regression method that we *have not* covered in class.
+Put these in Kaggle Kernels(can be one big one if you want) but don't make them public until after the competition is over.
+
+'''
+import scikit 
+import pandas
+import numpy
+import tensorflow as tf 
+
+
